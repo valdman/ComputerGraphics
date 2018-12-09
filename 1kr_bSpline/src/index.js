8c8604ddd7928b8d9e374ea6080094db42bb2c09
Boris Valdman <boris.valdman@live.ru>
2018-12-09 13:53:54 +0300

    animation added

--- 1kr_bSpline/src/index.js
+++ 1kr_bSpline/src/index.js
@@ -12,9 +12,10 @@ const parabola = d3.range(-10, 10.5, 0.5).map(x => ({
     y: x * x
 }))
 
+let bxy = [].concat(Bxy);
 const basisFunctionsData = generateBasisData();
 const splineData = generateSplineData();
-const data = basisFunctionsData.concat(splineData).concat(Bxy.map((v) => ({
+const data = basisFunctionsData.concat(splineData).concat(bxy.map((v) => ({
     x: v[0],
     y: v[1]
 })));
@@ -55,7 +56,7 @@ const colors = [
     "#BA690C"
 ]
     
-svg.append("g").selectAll("g.circles")
+svg.append("g").attr("class", "basis").selectAll("g.circles")
     .data(basisFunctionsData).enter()
     .append("circle")
     .attr("r", 0.8)
@@ -63,18 +64,68 @@ svg.append("g").selectAll("g.circles")
     .attr("cy", function (d) {return y(d.y*20)})
     .attr("fill", function (_, i) {return colors[Math.ceil(i / 600.0)]});
 
-svg.append("g").selectAll("g.circles")
+const t = d3.transition()
+      .duration(750);
+
+svg.append("g").attr("class", "spline").selectAll("g.circles")
     .data(splineData).enter()
     .append("circle")
     .attr("r", 0.8)
+.transition(t)
     .attr("cx", function (d) {return x(d.x)})
     .attr("cy", function (d) {return y(d.y)})
     .attr("fill", "black");
 
-svg.append("g").selectAll("g.circles")
-    .data(Bxy).enter()
+svg.append("g").attr("class", "dots").selectAll("g.circles")
+    .data(bxy).enter()
     .append("circle")
     .attr("r", 3)
+.transition(t)
     .attr("cx", function (d) {return x(d[0])})
     .attr("cy", function (d) {return y(d[1])})
     .attr("fill", "red");
+
+runUpdate("g.spline circle", 1600, (points) => {
+    points.transition()
+        .duration(500)
+        .attr("cx",function(d,i){
+            return x(d.x)
+        })
+        .attr("cy",function(d,i){
+            return y(d.y)
+        });
+},() => {
+    const newPoints = generateSplineData(bxy);
+    return newPoints;
+})
+
+const step = d3.randomNormal(0, 5);
+runUpdate("g.dots circle", 1500, (points) => {
+    points.transition()
+        .duration(500)
+        .attr("cx",function(d,i){
+            return x(d[0])
+        })
+        .attr("cy",function(d,i){
+            return y(d[1])
+        });
+}, () => {
+    const newPoints = bxy.map(v => [v[0]+step(), v[1]+step()])
+    bxy = newPoints;
+    return newPoints;
+})
+
+function runUpdate(selection, time, update, newData) {
+    const act = () => {
+        const points = svg
+        .selectAll(selection)
+        .data(newData);
+    
+        points.exit().remove();
+        points.enter().append("circle")
+            .attr("r",0);
+        
+        update(points);
+    }
+    setInterval(() => act(), time);
+}
--- 1kr_bSpline/src/mathStolen.js
+++ 1kr_bSpline/src/mathStolen.js
@@ -1,14 +1,14 @@
 export const Bxy = [ 
-    [ 0, 60 ],
-    [ 30, 0 ],
-    [ 60, 60 ],
-    [ 90, 0 ],
-    [ 120, 60 ],
-    [ 150, 0 ],
-    [ 180, 60 ],
-    [ 210, 0 ]
+    [ 0, 40 ],
+    [ 30, 20 ],
+    [ 60, 40 ],
+    [ 90, 20 ],
+    [ 120, 40 ],
+    [ 150, 20 ],
+    [ 180, 40 ],
+    [ 210, 20 ]
 ];
-const x = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+const x = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ]
 
 function RecurMetod(ind, k, par)
 {
@@ -27,7 +27,7 @@ function RecurMetod(ind, k, par)
         return ((par - x[ind]) * RecurMetod(ind, k - 1, par)) / (x[ind + k - 1] - x[ind]) + ((x[ind + k] - par) * RecurMetod(ind + 1, k - 1, par)) / (x[ind + k] - x[ind + 1]);
 }
 
-export function generateSplineData() {
+export function generateSplineData(a = Bxy) {
     let N, x,y;
     let res = []
     for (let p = 2; p <= 8; p = p + 0.01)
@@ -36,8 +36,8 @@ export function generateSplineData() {
         for (let i = 0; i < 8; i++)
         {
             N = RecurMetod(i, 3, p);
-            x = x + Bxy[i][0] * N;
-            y = y + Bxy[i][1] * N;                   
+            x = x + a[i][0] * N;
+            y = y + a[i][1] * N;                   
         }
         res.push({x, y})
     }
