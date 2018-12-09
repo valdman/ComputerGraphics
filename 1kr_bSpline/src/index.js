65c5f5c1bd35e96d0ea29d76727edadc257d582a
Boris Valdman <boris.valdman@live.ru>
2018-12-09 14:14:07 +0300

    d3 data-attr now lambdas

--- 1kr_bSpline/src/index.js
+++ 1kr_bSpline/src/index.js
@@ -60,9 +60,9 @@ svg.append("g").attr("class", "basis").selectAll("g.circles")
     .data(basisFunctionsData).enter()
     .append("circle")
     .attr("r", 0.8)
-    .attr("cx", function (d) {return x(d.x*36-70)})
-    .attr("cy", function (d) {return y(d.y*20)})
-    .attr("fill", function (_, i) {return colors[Math.ceil(i / 600.0)]});
+    .attr("cx", (d) => x(d.x*36-70))
+    .attr("cy", (d) => y(d.y*20))
+    .attr("fill",  (_, i) => colors[Math.ceil(i / 600.0)]);
 
 const t = d3.transition()
       .duration(750);
@@ -72,8 +72,8 @@ svg.append("g").attr("class", "spline").selectAll("g.circles")
     .append("circle")
     .attr("r", 0.8)
 .transition(t)
-    .attr("cx", function (d) {return x(d.x)})
-    .attr("cy", function (d) {return y(d.y)})
+    .attr("cx", (d) => x(d.x))
+    .attr("cy", (d) => y(d.y))
     .attr("fill", "black");
 
 svg.append("g").attr("class", "dots").selectAll("g.circles")
@@ -81,19 +81,15 @@ svg.append("g").attr("class", "dots").selectAll("g.circles")
     .append("circle")
     .attr("r", 3)
 .transition(t)
-    .attr("cx", function (d) {return x(d[0])})
-    .attr("cy", function (d) {return y(d[1])})
+    .attr("cx", (d) => x(d[0]))
+    .attr("cy", (d) => y(d[1]))
     .attr("fill", "red");
 
 runUpdate("g.spline circle", 1600, (points) => {
     points.transition()
         .duration(500)
-        .attr("cx",function(d,i){
-            return x(d.x)
-        })
-        .attr("cy",function(d,i){
-            return y(d.y)
-        });
+        .attr("cx", (d) => x(d.x))
+        .attr("cy", (d) => y(d.y));
 },() => {
     const newPoints = generateSplineData(bxy);
     return newPoints;
@@ -103,12 +99,8 @@ const step = d3.randomNormal(0, 5);
 runUpdate("g.dots circle", 1500, (points) => {
     points.transition()
         .duration(500)
-        .attr("cx",function(d,i){
-            return x(d[0])
-        })
-        .attr("cy",function(d,i){
-            return y(d[1])
-        });
+        .attr("cx", (d) => x(d[0]))
+        .attr("cy", (d) => y(d[1]))
 }, () => {
     const newPoints = bxy.map(v => [v[0]+step(), v[1]+step()])
     bxy = newPoints;
